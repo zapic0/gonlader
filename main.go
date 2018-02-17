05b30e0fd4442ed902ae7d3589c688674508369d
Zapico <zapico@gmail.com>
2018-02-17 21:30:46 +0100

    Fix downloads and change messages

--- main.go
+++ main.go
@@ -24,22 +24,24 @@ func main() {
 func parseWeb(fullURL string, cliArgs []string) {
 	pageParam := cliArgs[1]
 
-	lastPage, err := strconv.Atoi(cliArgs[2])
+	firstPage, err := strconv.Atoi(cliArgs[2])
+	lastPage, err := strconv.Atoi(cliArgs[3])
 
 	if err != nil {
-		fmt.Println("Last page param error")
+		fmt.Println("----- Last page param error")
 		os.Exit(-1)
 	}
 
-	for i := 1; i <= lastPage; i++ {
-		fmt.Println("NEW PAGE ----------------")
-
+	for i := firstPage; i <= lastPage; i++ {
 		currentPageURL := fullURL + "?" + pageParam + strconv.Itoa(i)
+		fmt.Println(" ------------------- " + strconv.Itoa(i))
+		fmt.Println("Getting page " + currentPageURL)
+		fmt.Println(" ------------------- " + strconv.Itoa(i) + "\n")
 
 		page, err := goquery.NewDocument(currentPageURL)
 
 		if err != nil {
-			fmt.Println("Error trying to get " + currentPageURL)
+			fmt.Println("----- Error trying to get " + currentPageURL)
 		}
 
 		parsePage(page, cliArgs)
@@ -47,10 +49,10 @@ func parseWeb(fullURL string, cliArgs []string) {
 }
 
 func parsePage(page *goquery.Document, cliArgs []string) {
-	host := cliArgs[3]
-	classToSearch := cliArgs[4]
-	fileClass := cliArgs[5]
-	fileAttr := cliArgs[6]
+	host := cliArgs[4]
+	classToSearch := cliArgs[5]
+	fileClass := cliArgs[6]
+	fileAttr := cliArgs[7]
 
 	page.Find(classToSearch).Each(func(i int, s *goquery.Selection) {
 		playValue, _ := s.Find(fileClass).First().Attr(fileAttr)
@@ -61,35 +63,39 @@ func parsePage(page *goquery.Document, cliArgs []string) {
 		}
 
 		name := s.Find("a").Text()
-		fmt.Println(fileURL + " " + name)
+		fmt.Println(fileURL)
 
-		downloadFile(file, name)
+		downloadFile(fileURL, name, cliArgs)
 	})
 
 }
 
-func downloadFile(url string, name string) {
-	err := os.Mkdir(name, os.ModePerm)
+func downloadFile(url string, name string, cliArgs []string) {
+	basicFolder := cliArgs[8]
+	err := os.Mkdir(filepath.Join("sounds", name), os.ModePerm)
+
+	remoteFile, err := http.Get(url)
 	if err != nil {
-		fmt.Println("Couldn't create folder for " + name)
-	} else {
-		remoteFile, err := http.Get(url)
-		if err != nil {
-			log.Println(err)
-		}
-
-		fileChunks := strings.Split(name, "/")
-		fileName := fileChunks[len(fileChunks)-1]
-
-		defer remoteFile.Body.Close()
-
-		out, err := os.Create(filepath.Join(name, fileName))
-
-		if err != nil {
-			log.Println(err)
-		}
-		defer out.Close()
-
-		io.Copy(out, remoteFile.Body)
+		log.Println(err)
+		fmt.Println("----- Error getting page " + url)
 	}
+
+	fileChunks := strings.Split(url, "/")
+	fileName := fileChunks[len(fileChunks)-1]
+
+	defer remoteFile.Body.Close()
+
+	fileFullPath := filepath.Join(basicFolder, filepath.Join(name, fileName))
+
+	fmt.Println(fileFullPath + "\n")
+
+	out, err := os.Create(fileFullPath)
+
+	if err != nil {
+		log.Println(err)
+		fmt.Println("----- Error creating / downloading file " + url)
+	}
+	defer out.Close()
+
+	io.Copy(out, remoteFile.Body)
 }
